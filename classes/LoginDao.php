189ea57c4c76d1278c82fccb487265edbfc7adc6
octavioreyes1 <octavioreyes1@hotmail.com>
2014-03-03 12:48:14 -0600

    Change the commands mysqli in LoginDao

--- classes/LoginDao.php
+++ classes/LoginDao.php
@@ -14,8 +14,8 @@ function findLogin($usuario, $password) {
       $id=null;
       $sql="select u.id"
          . " from Usuarios u where u.username='$usuario' and u.password = '".md5($password)."'";                 
-      $result = mysqli_query($sql,$this->connDb);              
-      while ($row = mysqli_fetch_array($result)){
+      $result = mysqli_query($this->connDb, $sql);              
+      while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
          $id = $row['id'];
       }  
       return $id;
